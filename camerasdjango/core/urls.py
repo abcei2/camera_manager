3806ee94575cd00f8465511151bfc3f93c2fb450
Jose B <jose.zdy@gmail.com>
2020-04-13 11:55:02 -0500

    oops. dont forget js translations

--- camerasdjango/core/urls.py
+++ camerasdjango/core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from django.views.i18n import JavaScriptCatalog
 
+
 urlpatterns = [
     path('', RedirectView.as_view(url='/cameras/manage_cameras'), name='home'),
     path('cameras/', include(
