5568a10814a1d1d86f239b91a558e74bac429159
Jose B <jose.zdy@gmail.com>
2020-04-20 18:10:58 -0500

    reverse url

--- camerasdjango/core/urls.py
+++ camerasdjango/core/urls.py
@@ -4,12 +4,18 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from django.views.i18n import JavaScriptCatalog
+from django.urls import reverse
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/cameras/manage_cameras'), name='home'),
-    path('cameras/', include(
-        ('cameras.urls', 'cameras'), namespace='cameras')),
+    path(
+        '',
+        RedirectView.as_view(url=reverse('cameras:manage_cameras'), name='home'
+    ),
+    path(
+        'cameras/',
+        include(('cameras.urls', 'cameras'), namespace='cameras')
+    ),
     path(
         'face_recognition/',
         include(
@@ -18,9 +24,9 @@ urlpatterns = [
         )
     ),
     path('admin/', admin.site.urls),
-
-    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog')
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(
@@ -28,6 +34,6 @@ if settings.DEBUG:
 
 if settings.ENABLE_DEBUG_TOOLBAR:
     import debug_toolbar
-    urlpatterns = [
+    urlpatterns=[
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
